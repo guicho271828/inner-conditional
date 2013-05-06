e0dd9da4e998c1405060f4866130d47a28d27837
Masataro Asai <guicho2.71828@gmail.com>
2013-05-06 14:53:43 +0900

    changed tab width

--- src/inner-conditional.lisp
+++ src/inner-conditional.lisp
@@ -1,6 +1,6 @@
 #|
-  This file is a part of inner-conditional project.
-  Copyright (c) 2013 Masataro Asai (guicho2.71828@gmail.com)
+This file is a part of inner-conditional project.
+Copyright (c) 2013 Masataro Asai (guicho2.71828@gmail.com)
 |#
 
 (in-package :cl-user)
@@ -13,8 +13,8 @@
 
 @export
 (defmacro with-inner ((&whole args
-							  label)
-					  &body body)
+			      label)
+		      &body body)
   @ignore label
   (call-with-inner args body))
 
@@ -24,54 +24,54 @@
 
 (defun walk-tree (fn tree)
   (funcall fn tree
-		   (lambda (branch)
-			 (mapcar (lambda (branch)
-					   (walk-tree fn branch))
-					 branch))))
+	   (lambda (branch)
+	     (mapcar (lambda (branch)
+		       (walk-tree fn branch))
+		     branch))))
 
 (defun precompile-directives (form &optional env)
   (walk-tree
    (lambda (subform cont)
-	 (iter (with expanded = subform)
-		   (when (member (typecase expanded
-						   (cons (car expanded))
-						   (symbol expanded))
-						 *precompiling-directives*)
-			 (setf expanded (macroexpand-1 expanded env))
-			 (next-iteration))
-		   (return (if (consp expanded)
-					   (funcall cont expanded)
-					   expanded))))
+     (iter (with expanded = subform)
+	   (when (member (typecase expanded
+			   (cons (car expanded))
+			   (symbol expanded))
+			 *precompiling-directives*)
+	     (setf expanded (macroexpand-1 expanded env))
+	     (next-iteration))
+	   (return (if (consp expanded)
+		       (funcall cont expanded)
+		       expanded))))
    form))
 
 (defun precompile-directives-1 (form &optional env)
   (walk-tree
    (let ((first t))
-	 (lambda (subform cont)
-	   (if first
-		   (iter (with expanded = subform)
-				 (when (member (typecase expanded
-								 (cons (car expanded))
-								 (symbol expanded))
-							   *precompiling-directives*)
-				   (setf first nil
-						 expanded (macroexpand-1 expanded env))
-				   (next-iteration))
-				 (return (if (consp expanded)
-							 (funcall cont expanded)
-							 expanded)))
-		   subform)))
+     (lambda (subform cont)
+       (if first
+	   (iter (with expanded = subform)
+		 (when (member (typecase expanded
+				 (cons (car expanded))
+				 (symbol expanded))
+			       *precompiling-directives*)
+		   (setf first nil
+			 expanded (macroexpand-1 expanded env))
+		   (next-iteration))
+		 (return (if (consp expanded)
+			     (funcall cont expanded)
+			     expanded)))
+	   subform)))
    form))
 
 (defun precompile-1-layer (sym fn form)
   (walk-tree
    (lambda (subform cont)
-	 (if (and (consp subform)
-			  (equalp sym (car subform)))
-		 (apply fn (cdr subform))
-		 (if (consp subform)
-			 (funcall cont subform)
-			 subform)))
+     (if (and (consp subform)
+	      (equalp sym (car subform)))
+	 (apply fn (cdr subform))
+	 (if (consp subform)
+	     (funcall cont subform)
+	     subform)))
    form))
 
 
@@ -82,8 +82,8 @@ is wrapping it. It just ignores =label= and put the body
 where it is originally located."
 (defmacro inner ((label) &body body)
   `(macrolet ((,label (&rest sexp)
-				`(progn ,@sexp)))
-	 ,@body))
+		`(progn ,@sexp)))
+     ,@body))
 
 (defpattern when (condition body)
   `(list* 'when ,condition ,body))
@@ -91,69 +91,69 @@ where it is originally located."
   `(list 'if ,condition ,then ,else))
 (defpattern cond (clauses)
   `(list 'cond
-		 ,@(mapcar (lambda (clause)
-					 `(list* ,@clause)) clauses)))
+	 ,@(mapcar (lambda (clause)
+		     `(list* ,@clause)) clauses)))
 
 (defpattern inner (label body)
   `(list 'inner (list (eq ,label))
-		 ,body))
+	 ,body))
 
 @doc "check whether =elem= is a =inner= clause.
 convert =when= if necessary."
 (defun match-inner (label elem)
   (match elem
-	((inner label (when cond body))
-	 `(if ,cond
-		  ,@body
-		  (,label nil)))
-	((inner label body) body)))
+    ((inner label (when cond body))
+     `(if ,cond
+	  ,@body
+	  (,label nil)))
+    ((inner label body) body)))
 
 (defun convert-first-inner-to-tag (body tag label)
   (let ((first t) conditional-body macrolet-body)
-	(setf macrolet-body
-		  (subst-if
-		   `(,tag)
-		   #'(lambda (elem)
-			   (when first
-				 (let ((body (match-inner label elem)))
-				   (when body
-					 (setf first nil conditional-body body)
-					 t))))
-		   (precompile-directives body)))
-	(values first conditional-body macrolet-body)))
+    (setf macrolet-body
+	  (subst-if
+	   `(,tag)
+	   #'(lambda (elem)
+	       (when first
+		 (let ((body (match-inner label elem)))
+		   (when body
+		     (setf first nil conditional-body body)
+		     t))))
+	   (precompile-directives body)))
+    (values first conditional-body macrolet-body)))
 
 ;; (defun call-with-macrolet 
 
 (defun call-with-inner (args body)
   (destructuring-bind (label &rest rest) args
-	@ignore rest
-	(with-gensyms (tag)
-	  (multiple-value-bind (first conditional-body macrolet-body)
-		  (convert-first-inner-to-tag body tag label)
-		(if first
-			`(progn ,@body)
-			(precompile-1-layer
-			 label
-			 (lambda (&rest sexp)
-			   (precompile-1-layer
-				tag (lambda (&rest args)
-					  @ignore args
-					  `(progn ,@sexp))
-				`(with-inner (,label)
-				   ,@macrolet-body)))
-			 conditional-body))))))
+    @ignore rest
+    (with-gensyms (tag)
+      (multiple-value-bind (first conditional-body macrolet-body)
+	  (convert-first-inner-to-tag body tag label)
+	(if first
+	    `(progn ,@body)
+	    (precompile-1-layer
+	     label
+	     (lambda (&rest sexp)
+	       (precompile-1-layer
+		tag (lambda (&rest args)
+		      @ignore args
+		      `(progn ,@sexp))
+		`(with-inner (,label)
+		   ,@macrolet-body)))
+	     conditional-body))))))
 
 @eval-always
 @export
 (defmacro define-inner-conditional
-	(name label macro-lambda-list &body body)
+    (name label macro-lambda-list &body body)
   `(progn 
-	 @eval-always
-	 (pushnew ',name *precompiling-directives*)
-	 @eval-always
-	 (defmacro ,name (,label ,@macro-lambda-list)
-	   `(inner (,label)
-		  ,,@body))))
+     @eval-always
+     (pushnew ',name *precompiling-directives*)
+     @eval-always
+     (defmacro ,name (,label ,@macro-lambda-list)
+       `(inner (,label)
+	  ,,@body))))
 
 
 
